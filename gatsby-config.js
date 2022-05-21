f949d0fbabd1ab009a6491e42bd5ec07a1e5adfe
zbjing <105762880+zbjing@users.noreply.github.com>
2022-05-21 09:41:25 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  pathPrefix: '/running', // Change to `/running_page` when running on github pages
+  pathPrefix: '/running_page', // Change to `/running_page` when running on github pages
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://zbjing.github.io/running/',
+    siteUrl: 'https://zbjing.github.io//running_page/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://zbjing.github.io/running/',
+        url: 'https://zbjing.github.io//running_page/',
       },
       {
         name: 'About',
-        url: 'https://zbjing.github.io/running/',
+        url: 'https://zbjing.github.io//running_page/',
       },
     ],
   },
